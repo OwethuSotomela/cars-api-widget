6ae8a74ac60017279df76df181e676250459019a
OwethuSotomela <owethusotomela@gmail.com>
2022-03-29 15:24:12 +0200

    Initial widget

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let carsElements = document.querySelector(".cars");
+
+async function getCars() {
+  const result = await axios.get("http://api-tutor.herokuapp.com/v1/cars");
+  result.data.forEach((car) => {
+    const li = document.createElement("tr");
+    li.innerHTML = `
+    <td>${car.model}</td>
+    <td>${car.model}</td>
+    <td>${car.color}</td>
+    `;
+    carsElements.appendChild(li);
+  });
+}
+
+getCars();
+
