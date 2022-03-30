61d07bda5fa7c9757019e17d8deaa6f35198d2da
OwethuSotomela <owethusotomela@gmail.com>
2022-03-30 15:35:28 +0200

    Getting make and color

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 let carsElements = document.querySelector(".cars");
-let carMake = document.querySelector(".carMake");
-let carModel = document.querySelector(".carModel");
-let carColor = document.querySelector(".carColor");
+
 let displaySelectedCar = document.querySelector(".displaySelectedCar");
 let submitBtn = document.querySelector(".submitBtn");
+let apiWidget = document.querySelector(".apiWidget");
+
+let templateSource = document.querySelector(".carTemplate").innerHTML;
+let carTemplate = Handlebars.compile(templateSource);
 
 async function getCars() {
   const result = await axios.get("https://api-tutor.herokuapp.com/v1/cars");
+  console.log(result);
   result.data.forEach((car) => {
     const li = document.createElement("tr");
     li.innerHTML = `
@@ -20,15 +23,22 @@ async function getCars() {
 
 getCars();
 
-function getSelectedCars() {
-  let getMake = carMake.value;
+async function getSelectedCars() {
+  let getMake = document.getElementById("carMake").value;
   console.log(getMake);
-
-  let getModel = carModel.value;
-  console.log(getModel);
-
-  let getColor = carColor.value;
+  let getColor = document.getElementById("carColor").value;
   console.log(getColor);
+  const result = await axios.get(
+    "http://api-tutor.herokuapp.com/v1/cars"
+  );
+  data = result.data;
+  console.log(data);
+  data = data.filter((car) => {
+    return car.make == getMake && car.color == getColor;
+  });
+  console.log(data);
+  displaySelectedCar.innerHTML = carTemplate({ model: data });
 }
+// getSelectedCars()
 
 submitBtn.addEventListener("click", getSelectedCars);
