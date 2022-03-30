88e0e8480705f1092c4cc0efed70f138b51ea5f1
OwethuSotomela <owethusotomela@gmail.com>
2022-03-30 10:12:31 +0200

    Option to choose a car

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 let carsElements = document.querySelector(".cars");
+let carMake = document.querySelector(".carMake");
+let carModel = document.querySelector(".carModel");
+let carColor = document.querySelector(".carColor");
+let displaySelectedCar = document.querySelector(".displaySelectedCar");
+let submitBtn = document.querySelector(".submitBtn");
 
 async function getCars() {
   const result = await axios.get("https://api-tutor.herokuapp.com/v1/cars");
@@ -15,3 +20,15 @@ async function getCars() {
 
 getCars();
 
+function getSelectedCars() {
+  let getMake = carMake.value;
+  console.log(getMake);
+
+  let getModel = carModel.value;
+  console.log(getModel);
+
+  let getColor = carColor.value;
+  console.log(getColor);
+}
+
+submitBtn.addEventListener("click", getSelectedCars);
